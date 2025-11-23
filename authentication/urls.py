b036c65b82248ab48ce61ff1f92a39511f5a3ef4
icethetic <ammarmuhraf@gmail.com>
2025-11-23 09:28:29 +0700

    Remove accounts app and update authentication system

---
+++ authentication/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from apps.authentication.views import register, login_user, logout_user
+
+app_name = 'authentication'
+
+urlpatterns = [
+    path('register/', register, name='register'),
+    path('login/', login_user, name='login'),
+]
--- authentication/views.py
+++ authentication/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 
-# Create your views here.
 def register(request):
     form = UserCreationForm()
 
@@ -14,3 +14,26 @@ def register(request):
             return redirect('main:login')
     context = {'form':form}
     return render(request, 'register.html', context)
+
+def login_user(request):
+   if request.method == 'POST':
+      form = AuthenticationForm(data=request.POST)
+
+      if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('main:show_main')
+
+   else:
+      form = AuthenticationForm(request)
+   context = {'form': form}
+   return render(request, 'login.html', context)
+
+@login_required
+def logout_user(request):
+    logout(request)
+    messages.success(request, 'You have been logged out successfully!')
+    return redirect('main:login')
+
+
+
