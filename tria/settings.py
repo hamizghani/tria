1de9c9b8169860ae918a37ed1fd1bf86a6f7d700
hamizghaniii <hamizghaniiii@gmail.com>
2025-11-26 21:29:32 +0700

    feat: pws mantap

--- tria/settings.py
+++ tria/settings.py
@@ -138,9 +138,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [
-    BASE_DIR / "tria" / "static",  # if you’re using app-level static files
+    BASE_DIR / 'static'
 ]
 
 # Default primary key field type
